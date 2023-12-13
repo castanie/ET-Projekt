fb99713a6f44b40a0830817093c420ea1719490f
Daniel Egger <daniel.egger@plincs.com>
2023-12-13 14:51:18 +0100

    Generate project

--- Projekt 1/src/main/java/ab1/exceptions/IllegalAcceptingStateException.java
+++
@@ -1,7 +0,0 @@
-package ab1.exceptions;
-
-public class IllegalAcceptingStateException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- Projekt 1/src/main/java/ab1/exceptions/IllegalInitialStateException.java
+++
@@ -1,7 +0,0 @@
-package ab1.exceptions;
-
-public class IllegalInitialStateException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
